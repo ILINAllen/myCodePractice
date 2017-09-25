7e718a3f2c4436ec6612486e56f4e70a5364f124
ILINAllen <linzhaolun@yeah.net>
2017-09-25 23:47:51 +0800

    add a practice code

---
+++ 3.1.2quanpailie.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <stdio.h>
+#include<algorithm>
+using namespace std;
+void perm(int list[],int m,int begin)
+{
+	if(begin==m)
+	{
+		for(int i=0;i<m;i++)
+			cout<<list[i];
+		cout<<endl;	
+	}
+	for(int j=begin;j<m;j++)
+	{
+		swap(list[begin],list[j]);
+		perm(list,m,begin+1);
+		swap(list[begin],list[j]);
+	}	
+}
+
+
+
+
+int main()
+{
+	int list[]={1,2,3};
+	
+	perm(list,3,0);
+	return 0;
+
+}
